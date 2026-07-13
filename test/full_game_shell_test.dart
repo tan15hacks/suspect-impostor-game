c1945abb284bd930a8a56b343287a727d2c4a354
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-13 09:48:23 +0800

    Remove unused widget-test import

--- test/full_game_shell_test.dart
+++ test/full_game_shell_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:suspect_impostor_game/app/full_game_app.dart';
 import 'package:suspect_impostor_game/app/full_game_controller.dart';
