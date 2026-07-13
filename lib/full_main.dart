0983db101dee358993705a1f1920d938e4480818
tan15hacks <110644530+tan15hacks@users.noreply.github.com>
2026-07-13 09:51:03 +0800

    Load persistent local state before launching full shell

--- lib/full_main.dart
+++ lib/full_main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'app/full_game_app.dart';
+import 'app/full_game_controller.dart';
 
-void main() {
-  runApp(const FullSuspectGameApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final controller = FullGameController(enablePersistence: true);
+  await controller.loadFromStorage();
+  runApp(FullSuspectGameApp(controller: controller));
 }
